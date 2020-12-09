21370a6f7a018f157804280228a9b6e9eb173ba0
martynas-stytch <74532709+martynas-stytch@users.noreply.github.com>
2020-12-09 19:04:46 +0200

    Update AppDelegate.swift

--- StytchExample/StytchExampleUI/AppDelegate.swift
+++ StytchExample/StytchExampleUI/AppDelegate.swift
@@ -13,8 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    let stytchProjectID = "project-test-d0dbafe6-a019-47ea-8550-d021c1c76ea9"
-    let stytchSecretKey = "secret-test-6-ma0PNENqjBVX6Dx2aPUIdhLFObauXx07c="
+    let stytchProjectID = "ProjectID"
+    let stytchSecretKey = "SecretKey"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
